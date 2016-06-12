bf923e0b8de4c388e2b35a2d66e294051a6b1519
kreyesdr <kreyes616@gmail.com>
2016-06-12 18:14:32 -0400

    Done Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,30 @@
 require 'random_data'
 
-# Create Posts
 50.times do
-  # #1
+
   Post.create!(
-      # #2
+
       title:  RandomData.random_sentence,
       body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
 
-# Create Comments
-# #3
 100.times do
   Comment.create!(
-      # #4
+
       post: posts.sample,
       body: RandomData.random_paragraph
   )
 end
+
+puts "#{Post.count}"
+Post.find_or_create_by(title: 'Bloccit Title Unique', body: 'Bloccit Body Unique')
+puts "#{Post.count}"
+
+puts "#{Comment.count}"
+Comment.create_with(body: 'Bloccit Comment Unique').find_or_create_by(post: posts.sample)
+puts "#{Comment.count}"
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
