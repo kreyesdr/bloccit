0861164744153ce712c00a5660911bdc2e7b03cf
kreyesdr <kreyes616@gmail.com>
2016-06-14 22:02:59 -0400

    Finish the checkpoint, good knowledge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,15 @@
 require 'random_data'
 
-# Create Posts
 50.times do
-  # #1
   Post.create!(
-      # #2
       title:  RandomData.random_sentence,
       body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
 
-# Create Comments
-# #3
 100.times do
   Comment.create!(
-      # #4
       post: posts.sample,
       body: RandomData.random_paragraph
   )
