320846b592523bdc3f11f97958c3caf50643b0d5
kreyesdr <kreyes616@gmail.com>
2016-06-18 13:23:47 -0400

    Done Checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    @posts.each_with_index { |post, index |
+    @post = Post.all
+    @post.each_with_index { |post, index |
       if index % 5 == 0
         post.title = "SPAM"
       end
@@ -9,9 +9,25 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:posts][:title]
+    @posts.body = params[:posts][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved successfully"
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again"
+      render :new
+    end
   end
 
   def edit
