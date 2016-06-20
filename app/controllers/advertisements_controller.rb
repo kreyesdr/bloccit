37224a5cae2f98b35281bd21ed2e1d054d24c10d
kreyesdr <kreyes616@gmail.com>
2016-06-20 15:24:04 -0400

    Done assignment

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -12,5 +12,17 @@ class AdvertisementsController < ApplicationController
   end
 
   def create
+    @advertisement = Advertisement.new
+    @advertisement.title = params[:advertisement][:title]
+    @advertisement.body = params[:advertisement][:body]
+    @advertisement.price = params[:advertisement][:price]
+
+    if @advertisement.save
+      flash[:notice] = 'Advertisement was saved'
+      redirect_to @advertisement
+    else
+      flash[:error] = 'There was an error saving the advertisement. Please try again.'
+      render :new
+    end
   end
 end
