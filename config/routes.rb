0c949fa5e2f1d66b06ac98dd141139331086164f
kreyesdr <kreyes616@gmail.com>
2016-06-25 17:05:49 -0400

    Done 34 Checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :advertisements
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
-  resources :posts
+  resources :advertisements
 
   get 'about' => 'welcome#about'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
 require 'random_data'
 
+15.times do
+  Topic.create!(
+      name: RandomData.random_sentence,
+      description: RandomData.random_paragraph
+  )
+end
+
+topics = Topic.all
+
 50.times do
   Post.create!(
-      title:  RandomData.random_sentence,
-      body:   RandomData.random_paragraph
+      topic: topics.sample,
+      title: RandomData.random_sentence,
+      body: RandomData.random_paragraph
   )
 end
 posts = Post.all
@@ -16,5 +26,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
