c48999fe67d0b185e6bc09c5174a47ff326df3dd
kreyesdr <kreyes616@gmail.com>
2016-07-05 20:27:59 -0400

    Adding avatar_url helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,4 +11,8 @@ module SessionsHelper
     User.find_by(id: session[:user_id])
   end
 
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdogest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
