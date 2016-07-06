ee66a49a7e22dcb6205f631ed5f98e584fbe7db8
kreyesdr <kreyes616@gmail.com>
2016-07-05 23:13:26 -0400

    Done 38 Assignment

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,7 +12,7 @@ module SessionsHelper
   end
 
   def avatar_url(user)
-    gravatar_id = Digest::MD5::hexdogest(user.email).downcase
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
   end
 end
