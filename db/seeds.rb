234a53da0733476dd6008450f91f95a83eb1e821
kreyesdr <kreyes616@gmail.com>
2016-07-23 16:42:54 -0400

    Done with votes,post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,12 +20,16 @@ end
 topics = Topic.all
 
 50.times do
-  Post.create!(
+  post = Post.create!(
       user: users.sample,
       topic: topics.sample,
       title: RandomData.random_sentence,
       body: RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times {post.votes.create!(value: [-1,1].sample, user: users.sample)}
+
 end
 
 30.times do
@@ -68,4 +72,5 @@ puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 puts "#{SponsoredPost.count} sponsored post created"
+puts "#{Vote.count} votes created"
 puts "---------------"
