fab803714b187093a5027b170577de3cf169cfb9
kreyesdr <kreyes616@gmail.com>
2016-07-28 15:00:14 -0400

    Almost done with assignment

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def non_post_text
+    if @user.posts.count === 0
+      "#{@user.name} has not submitted any posts yet."
+    else
+       render @user.posts
+    end
+  end
 end
