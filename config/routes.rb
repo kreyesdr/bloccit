2b5763e26f737c994d5cfdc3d000ed601a1e910f
kreyesdr <kreyes616@gmail.com>
2016-08-01 23:23:07 -0400

    Done 48 Checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,12 @@ Rails.application.routes.draw do
   get 'faq' => 'welcome#faq'
 
   root 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
+
 end
